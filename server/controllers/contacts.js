b3a5c6e5c0fd354376f17180fc0d085b29bd7724
Joshua Peters <jpeters280@gmail.com>
2017-01-27 15:06:14 -0800

    contacts update

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -1,19 +1,19 @@
-console.log("entering contacts controller");
 var nodemailer      = require('nodemailer');
-var transporter     = nodemailer.createTransport({
-        service: 'Hotmail',
-        auth: {
-            user: 'translucentduck@hotmail.com', // Your email id
-            pass: '21' // Your password
-        }
-    });
+var mg = require('nodemailer-mailgun-transport');
+var auth = {
+  auth: {
+    api_key: '',
+    domain: 'sandbox9f3c0ea7e1964c2d89a245c988c08c77.mailgun.org'
+  }
+}
+
+var nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
 module.exports = {
     create: function(req, res) {
         var newcontact = req.body;
-        transporter.sendMail(
-        	{ 	from: 'translucentduck@hotmail.com',
-                replyTo: newcontact.email,
+        nodemailerMailgun.sendMail(
+        	{ 	from: newcontact.email,
                 to: 'jpeters280@gmail.com',
                 subject: newcontact.subject,
                 text: 'from: ' + newcontact.firstName + ' ' + newcontact.lastName + "\r\n" + 'email: ' + newcontact.email + "\r\n" + 'Phone Number: ' + newcontact.phoneNumber + "\r\n\r\n" + newcontact.message
@@ -27,33 +27,3 @@ module.exports = {
     
 }
 
-
-// function contactsController(){
-//   this.index = function(req,res){
-//   	console.log("index method backend");
-
-//     //your code here
-//     res.json({placeholder:'index'});
-//   };
-//   this.create = function(req,res){
-//   	console.log("create method backend");
-//     //your code here
-//     res.json({placeholder:'create'});
-//   };
-//   this.update = function(req,res){
-//   	console.log("update method backend");
-//     //your code here
-//     res.json({placeholder:'update'});
-//   };
-//   this.delete = function(req,res){
-//   	console.log("delete method backend");
-//     //your code here
-//     res.json({placeholder:'delete'});
-//   };
-//   this.show = function(req,res){
-//   	console.log("show method backend");
-//     //your code here
-//     res.json({placeholder:'show'});
-//   };
-// }
-// module.exports = new contactsController(); // this exports an object
